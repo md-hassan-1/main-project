e075c489f99bd90c9fe8d981079ee90118e5c13e
md-hassan-1 <hassanansari8533@gmail.com>
2024-03-10 15:24:02 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,35 @@
+
+let nav = document.querySelector("#nav")
+         window.addEventListener("scroll",() => {
+        setInterval(function(){
+            nav.style.backgroundColor="rgba(0, 0, 0, 1)";
+        },300)
+
+    
+  
+
+    
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+    
+
+   
+    
+ 
